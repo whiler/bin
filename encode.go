e1b7be2e2ed5108d66532b188b3dd597c5ba539e
whiler <wenwu500@qq.com>
2018-07-30 15:45:20 +0800

    remove int/uint support

--- encode.go
+++ encode.go
@@ -126,8 +126,8 @@ func MarshalBigEndian(ins interface{}) ([]byte, error) {
 			_, err = buf.Write(data)
 
 		case reflect.Bool,
-			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+			reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
 			reflect.Float32, reflect.Float64,
 			reflect.Complex64, reflect.Complex128:
 			err = binary.Write(buf, binary.BigEndian, cur.Interface())
